1df94fb2168d5b7dd3b2e8a45e678144d84e224e
dhruvbaliyan <dhruvbaliyan1001@gmail.com>
2023-05-01 14:17:04 +0530

    chore: add member core

--- rusty-seed-database/src/lib.rs
+++ rusty-seed-database/src/lib.rs
@@ -1,5 +1,7 @@
 pub mod sleddb;
 pub mod error;
+use std::path::Path;
+
 use sleddb::SledDatabase;
 
 pub struct Database {
@@ -9,12 +11,11 @@ pub struct Database {
 #[allow(unused_variables)]
 impl Database {
     // Open and return existing database, if not present, it create and return a new one
-    fn open() {
-        todo!()
+    pub fn open(path: &Path) -> Self {
+        Database { sled: SledDatabase::open(path) }
     }
 
-    // creates a new database and return 
-    fn new() {
-        todo!()
+    pub fn add_file(&mut self, hash: String, path: &Path) {
+        self.sled.add_file(hash, path);
     }
 }
--- rusty-seed-server/src/main.rs
+++ rusty-seed-server/src/main.rs
@@ -1,7 +1,9 @@
-use file::reader::FileReader;
-
 pub mod file;
 
 fn main() {
-    
+    // TODO: start listening on a port for requests from cli
+    // TODO: load database(s)
+    // TODO: send request to client to continue uncomplete downloads
+    // TODO: verify downloads
+    // TODO: start seeding active paths
 }
